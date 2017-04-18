a463cf88e5a38a7edf845b904efd50cfcb9b9ad7
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-18 23:25:48 +0800

    Trigger an action when Users interact with UI

--- daily/ViewController.swift
+++ daily/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
 
+    var quotes = Quote()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func inspireMeDidTap(sender: UIButton)
+    {
+        let quote = quotes.randomQuote()
+        quoteLabel.text = quote
+        
+        let image = randomImage()
+        backgroundImageView.image = image
+        sender.backgroundImage = randomColor
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func randomImage() -> UIImage
+    {
+        let imageCount = 6
+        let randomNumber = Int(arc4random()) % imageCount + 1
+        
+        if let image = UIImage(named: "image\(randomNumber)") {
+            return image
+        } else {
+            return UIImage(named: "image1")!
+        }
+    }
+    
+   
 }
 
