64818702d4fdee07e4d52a43bb927a77baed589e
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-18 23:08:11 +0800

    Initial Commit

---
+++ daily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daily
+//
+//  Created by Popsical on 18/4/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
