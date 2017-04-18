35c049ea0da7409c7de9dac89a94ac746623a5b1
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-19 00:02:37 +0800

    Customer button background color

--- daily/ViewController.swift
+++ daily/ViewController.swift
@@ -20,14 +20,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func inspireMeDidTap(sender: UIButton)
-    {
+    @IBAction func inspireMeDidTap(_ sender: UIButton) {
         let quote = quotes.randomQuote()
         quoteLabel.text = quote
         
         let image = randomImage()
         backgroundImageView.image = image
-//        sender.backgroundImage = randomColor()
+        sender.backgroundColor = randomColor()
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,8 +34,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func randomImage() -> UIImage
-    {
+    func randomImage() -> UIImage {
         let imageCount = 6
         let randomNumber = Int(arc4random()) % imageCount + 1
         
@@ -47,5 +45,16 @@ class ViewController: UIViewController {
         }
     }
     
+    func randomColor() -> UIColor {
+        let random = Int(arc4random()) % 5  // 0 -> 4
+        switch random {
+        case 0: return UIColor(red: 211/255.0, green: 86/255.0, blue: 87/255.0, alpha: 0.9)
+        case 1: return UIColor(red: 71/255.0, green: 178/255.0, blue: 137/255.0, alpha: 0.9)
+        case 2: return UIColor(red: 229/255.0, green: 177/255.0, blue: 93/255.0, alpha: 0.9)
+        case 3: return UIColor(red: 92/255.0, green: 163/255.0, blue: 178/255.0, alpha: 0.9)
+        case 4: return UIColor(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 0.9)
+        default: return UIColor(red: 56/255.0, green: 72/255.0, blue: 72/255.0, alpha: 0.9)
+        }
+    }
 }
 
